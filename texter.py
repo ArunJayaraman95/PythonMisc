c651d48aaa396464a191416c01aa7ffc5cb250db
Arun Jayaraman <riparun.aj@gmail.com>
2022-03-14 01:25:32 -0400

    Initial Text Commit. Add .new env file (gitgnored)

---
+++ texter.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+def sendText(subject, body):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = os.getenv('CELL_EMAIL')
+    msg['from'] = os.getenv('MAIN_EMAIL')
+
+    user = os.getenv('MAIN_EMAIL')
+    password = os.getenv('EMAIL_APP_PASSWORD')
+    print(f'{password = }')
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+
+if __name__ == '__main__':
+    sendText("Daily Update", "\nHi Arun\nHere's your daily update!")
+    print("Sent text!")
