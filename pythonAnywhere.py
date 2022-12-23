815ce046bb65f891e85b978a9cd446a25595608b
Arun Jayaraman <riparun.aj@gmail.com>
2022-12-22 22:10:12 -0500

    Added random berserk quote with anime API

--- pythonAnywhere.py
+++ pythonAnywhere.py
@@ -2,10 +2,22 @@ CELL_EMAIL = "2482668138@tmomail.net"
 PASSWORD = "ktisdqxxyvcvgnwt"
 MAIN_EMAIL = "riparun.aj@gmail.com"
 
-import smtplib
+import smtplib, requests
 from email.message import EmailMessage
+from datetime import date
 
-# load_dotenv()
+def getBerserkQuote() -> tuple[str, str, str]:
+    """Returns tuple of anime character, show, and then
+    random quote"""
+    anime_url = "https://animechan.vercel.app/api/random/anime?title=berserk"
+    data = requests.get(anime_url).json()
+    character = data['character']
+    quote = data['quote']
+    anime = data['anime']
+    # sendText("Anime Quote")
+    return character, anime, quote
+
+
 def sendText(subject, body):
     msg = EmailMessage()
     msg.set_content(body)
@@ -22,12 +34,17 @@ def sendText(subject, body):
     server.starttls()
     server.login(user, password)
     server.send_message(msg)
-    print("Sent text!")
     server.quit()
 
 if __name__ == '__main__':
-    sendText("Daily Update", "\nLaptop: This is Arun the cool guy")
-    print("Sentd text!")
+    currentDate = date.today().strftime("%b-%d-%Y")
+    dailyQuote = getBerserkQuote()
+    body = f"\nLaptop Notification \
+        \n\nBerserk QotD: \
+        \n\"{dailyQuote[2]}\" ~ {dailyQuote[0]}"
+
+    sendText(f"Update for {currentDate}", body)
+    print("Sent text!")
 
 # TODO: Weather
 # TODO: Stock Changes
